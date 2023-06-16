786196b09c8a6a7809af09379a1a508886f7f183
Sofia Cruz <soffiact.97@gmail.com>
2023-06-15 21:35:35 -0600

    otra prueba

---
+++ Happybdayamor.py
@@ -0,0 +1,4 @@
+def how_much_i_love_you(Tommy):
+    if Tommy == "Bday":
+        return "Tommy<3" "\n" "¿Mi tierra? mi tierra eres tú," "\n"  "¿Mi gente? Mi gente eres tú," "\n" "El destierro y la muerte para mi están donde no estés tú," "\n" "¿Y mi vida? Dime, mi vida, ¿qué es, si no eres tú?" "\n" "Te amo con todo mi corazón, felices 25, feliz cumpleaños mí amor" "\n" "con amor Sofi"
+print(how_much_i_love_you( "Bday" ))
