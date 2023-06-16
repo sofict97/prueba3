56735b0f8942b4db46860c72c10cf371c8ec4921
Sofia Cruz <soffiact.97@gmail.com>
2023-06-15 22:01:48 -0600

    Seagrego int=12

--- Happybdayamor.py
+++ Happybdayamor.py
@@ -2,3 +2,6 @@ def how_much_i_love_you(Tommy):
     if Tommy == "Bday":
         return "Tommy<3" "\n" "¿Mi tierra? mi tierra eres tú," "\n"  "¿Mi gente? Mi gente eres tú," "\n" "El destierro y la muerte para mi están donde no estés tú," "\n" "¿Y mi vida? Dime, mi vida, ¿qué es, si no eres tú?" "\n" "Te amo con todo mi corazón, felices 25, feliz cumpleaños mí amor" "\n" "con amor Sofi"
 print(how_much_i_love_you( "Bday" ))
+print("te amo mucho toms jaja")
+
+int=12
